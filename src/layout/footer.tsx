b14f8c68572522da63507d8b9dacda172d82e521
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-01-29 13:11:55 +0100

    footer

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,41 +1,51 @@
-import { Layout } from "antd";
+import { Layout, Divider } from "antd";
+import { CSSProperties } from "react";
+import { grey } from '@ant-design/colors';
 
-import Ademe from "../../public/Logo_ADEME.svg";
-import Prefet from "../../public/Préfet_de_la_région_Hauts-de-France.svg";
-import Region from "../../public/Logo Région HDF.png";
-import Cerc from "../../public/Logo_CERC_Hauts-de-Fce_sans-sign.svg";
-import Cerdd from "../../public/Logo_cerdd.svg";
-import Geo2France from "../../public/geo2france.svg";
+import Ademe from "/img/Logo_ADEME.svg";
+import Prefet from "/img/Préfet_de_la_région_Hauts-de-France.svg";
+import Region from "/img/Logo Région HDF.png";
+import Cerc from "/img/Logo_CERC_Hauts-de-Fce_sans-sign.svg";
+import Cerdd from "/img/Logo_cerdd.svg";
+import Geo2France from "/img/geo2france.svg";
+import Odema from "/img/logo_odema.png";
 
 export const AppFooter: React.FC = () => {
-    const logos = [
-      {src : Ademe, name: "Ademe", url: "https://www.ademe.fr/"},
-      {src : Prefet, name: "Préfecture Hauts-de-France", url: "https://www.hauts-de-france.developpement-durable.gouv.fr/"},
-      {src : Region, name: "Région Hauts-de-France", url: "https://www.hautsdefrance.fr/"},
-      {src : Cerc, name: "CERC Hauts-de-France", url: "https://www.cerc-hautsdefrance.fr/"},
-      {src : Cerdd, name: "CERDD", url: "https://www.cerdd.org/"},
-      {src : Geo2France, name: "Géo2France", url: "https://www.geo2france.fr/"}
 
-    ]
-    return(
-        <Layout.Footer
-            style={{
-              textAlign: "center",
-              color: "#fff",
-              backgroundColor: "#fff",
-            }}
-          >
-             <div className="logo">
-              { logos.map((l) => 
-                <a href={l.url}> 
-                  <img style={{ 
-                      height:'70px',
-                      marginRight: '20px'
-                     }}
-                    src={l.src} alt={l.name}/>
-                  </a>
-              )}
-            </div>
-          </Layout.Footer>
-    )
+  const style_img:CSSProperties = { 
+    height:'70px',
+    marginRight: '20px'
+  }
+  console.log(grey)
+  const logos = [
+    {src : Ademe, name: "Ademe", url: "https://www.ademe.fr/"},
+    {src : Prefet, name: "Préfecture Hauts-de-France", url: "https://www.hauts-de-france.developpement-durable.gouv.fr/"},
+    {src : Region, name: "Région Hauts-de-France", url: "https://www.hautsdefrance.fr/"},
+    {src : Cerc, name: "CERC Hauts-de-France", url: "https://www.cerc-hautsdefrance.fr/"},
+    {src : Cerdd, name: "CERDD", url: "https://www.cerdd.org/"},
+    {src : Geo2France, name: "Géo2France", url: "https://www.geo2france.fr/"}
+
+  ]
+  return(
+      <Layout.Footer
+          style={{
+            textAlign: "center",
+            color: "#fff",
+            backgroundColor: "#fff",
+          }}
+        >
+            <div>
+            <a href="/">
+              <img style={style_img} src={Odema} alt="Odema"/>
+            </a>
+            <Divider type="vertical" style={{ height: "70px", backgroundColor: grey[2] }} />
+            { logos.map((l) => 
+              <a href={l.url}> 
+                <img style={style_img}
+                  src={l.src} alt={l.name}/>
+                </a>
+            )}
+          </div>
+        </Layout.Footer>
+  )
 }
