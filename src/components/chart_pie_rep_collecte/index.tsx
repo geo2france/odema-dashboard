02cac6d84452d541f59e522f395006baf1abdcd3
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-02-23 11:56:15 +0100

    add pa, dispmed, mnu

--- src/components/chart_pie_rep_collecte/index.tsx
+++ src/components/chart_pie_rep_collecte/index.tsx
@@ -13,18 +13,44 @@ interface ChartPieRepCollecteProps {
 
 export const ChartPieRepCollecte: React.FC<ChartPieRepCollecteProps> = ({data, filiere, annee, c_region='32'} )  => {
     let data_pie = []
-    //console.log(data)
-    if (filiere == 'd3e'){
+    if (filiere == 'd3e'){ //Les données des différentes fillière REP ne sont pas diffusée de manière homogène
         data_pie = alasql(`SELECT [Code_région], d.Flux, sum([Total]) AS tonnage
         FROM ? d
         GROUP BY [Code_région], d.Flux
         `, [data]).map((e:BaseRecord) => ({name: e.Flux, value: e.tonnage} ))
+    }else if (filiere == 'pa'){
+        data_pie = alasql(`
+        SELECT d.[Code_Région], 'Collectivités' AS type, sum(d.[Collectivités]) AS tonnage
+        FROM ? d
+        GROUP BY d.[Code_Région]
+        UNION ALL CORRESPONDING
+        SELECT d2.[Code_Région], 'Distribution' AS type, sum(d2.[Distribution]) AS tonnage
+        FROM ? d2
+        GROUP BY d2.[Code_Région]
+        UNION ALL CORRESPONDING
+        SELECT d3.[Code_Région], 'Autre' AS type, sum(d3.[Autre]) AS tonnage
+        FROM ? d3
+        GROUP BY d3.[Code_Région]
+        `, [data,data,data]).map((e:BaseRecord) => ({name: e.type, value: e.tonnage} ))
+        console.log(data)
+        console.log(data_pie)
+
     }else if (filiere == 'pchim'){
         data_pie = alasql(`SELECT [equip_declare], sum([Somme_de_masse]) AS tonnage
         FROM ? d
         GROUP BY [equip_declare]
         `, [data]).map((e:BaseRecord) => ({name: e.equip_declare, value: e.tonnage} ))
     }else if (filiere == 'tlc'){
+        data_pie = alasql(`SELECT [origine], sum([tonnage]) AS tonnage
+        FROM ? d
+        GROUP BY [origine]
+        `, [data]).map((e:BaseRecord) => ({name: e.origine, value: e.tonnage} ))
+    }else if (filiere == 'mnu'){
+        data_pie = alasql(`SELECT [Code_Région], sum([tonnage]) AS tonnage
+        FROM ? d
+        GROUP BY [Code_Région]
+        `, [data]).map((e:BaseRecord) => ({name: 'MNU', value: e.tonnage} ))
+    }else if (filiere == 'disp_med'){
         data_pie = alasql(`SELECT [origine], sum([tonnage]) AS tonnage
         FROM ? d
         GROUP BY [origine]
--- src/components/pages/rep.tsx
+++ src/components/pages/rep.tsx
@@ -1,5 +1,5 @@
 import { IResourceComponentsProps, useList } from "@refinedev/core";
-import { Row, Col, Card, Alert } from "antd";
+import { Row, Col, Card, Alert, List } from "antd";
 import { useSearchParamsState } from "../../utils";
 import { ChartPieRepCollecte } from "../chart_pie_rep_collecte";
 import { LoadingComponent } from "../loading_container";
@@ -78,6 +78,76 @@ export const RepPage: React.FC<IResourceComponentsProps> = () => {
         }
     )
 
+    const collecte_mnu = useList(
+        {
+            resource: "rep-mnu-tonnages-collectes-en-2021/lines",
+            dataProviderName: "ademe_opendata",
+            pagination: {
+                pageSize: 150,
+            },
+            filters: [
+                {
+                    field: "Code_Région",
+                    operator: "eq",
+                    value: cregion
+                },
+                {
+                    field: "Année_des_données",
+                    operator: "eq",
+                    value: year
+                },
+
+            ]
+        }
+    )
+
+    const collecte_disp_med = useList(
+        {
+            resource: "rep-disp-med-tonnages-collectes-en-2021/lines",
+            dataProviderName: "ademe_opendata",
+            pagination: {
+                pageSize: 150,
+            },
+            filters: [
+                {
+                    field: "Code_Dept",
+                    operator: "in",
+                    value: ['59','62','80','02','60']
+                },
+                {
+                    field: "Année_des_données",
+                    operator: "eq",
+                    value: year
+                },
+
+            ]
+        }
+    )
+
+    const collecte_pa = useList(
+        {
+            resource: "rep-pa-tonnages-collectes-en-2018/lines",
+            dataProviderName: "ademe_opendata",
+            pagination: {
+                pageSize: 150,
+            },
+            filters: [
+                {
+                    field: "Code_Région",
+                    operator: "eq",
+                    value: cregion
+                },
+                {
+                    field: "Année_des_données",
+                    operator: "eq",
+                    value: year
+                },
+
+            ]
+        }
+    )
+
+
 
     return(
         <>
@@ -101,6 +171,14 @@ export const RepPage: React.FC<IResourceComponentsProps> = () => {
                 </Col>
                 <Col xl={24/2} xs={24}>
                     <Card>
+                        pa
+                        <LoadingComponent isLoading={collecte_pa.isFetching}>
+                            {collecte_pa.data ? <ChartPieRepCollecte filiere='pa' data={collecte_pa.data.data} /> : <b>...</b>}
+                        </LoadingComponent>
+                    </Card>
+                </Col>
+                <Col xl={24/2} xs={24}>
+                    <Card>
                         pchim
                         <LoadingComponent isLoading={collecte_pchim.isFetching}>
                             {collecte_pchim.data ? <ChartPieRepCollecte filiere='pchim' data={collecte_pchim.data.data} /> : <b>...</b>}
@@ -115,6 +193,43 @@ export const RepPage: React.FC<IResourceComponentsProps> = () => {
                         </LoadingComponent>
                     </Card>
                 </Col>
+                <Col xl={24/2} xs={24}>
+                    <Card>
+                        tlc
+                        <LoadingComponent isLoading={collecte_mnu.isFetching}>
+                            {collecte_mnu.data ? <ChartPieRepCollecte filiere='mnu' data={collecte_mnu.data.data} /> : <b>...</b>}
+                        </LoadingComponent>
+                    </Card>
+                </Col>
+                <Col xl={24/2} xs={24}>
+                    <Card>
+                        tlc
+                        <LoadingComponent isLoading={collecte_disp_med.isFetching}>
+                            {collecte_disp_med.data ? <ChartPieRepCollecte filiere='disp_med' data={collecte_disp_med.data.data} /> : <b>...</b>}
+                        </LoadingComponent>
+                    </Card>
+                </Col>
+                <Col xl={24 / 2} xs={24}>
+                    <Card>
+                        <List
+                            size="small"
+                            header={<div><b>Autres données</b></div>}
+                            bordered
+                            dataSource={[
+                                'Quantité mise sur le marché : niveau national seulemnet',
+                                'DEA : données 2017-2020 seulement',
+                                'VHU : Taux de recyclage et de valorisation (par département et par an) seulement'
+                            ]}
+                            renderItem={(item) => <List.Item>{item}</List.Item>}
+                        />
+
+                    </Card>
+                </Col>
+
+
+
+
+
             </Row>
         </>
     )
