5bbd3daa343635da28b3f9f444b38122cc7439d2
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-03-06 09:38:01 +0100

    rep topbar menu

--- src/components/rep_topbar/index.tsx
+++ src/components/rep_topbar/index.tsx
@@ -1,13 +1,20 @@
 import { useMenu } from "@refinedev/core";
 import { Alert, Card, Menu, Select, Space } from "antd"
 import { NavLink } from "react-router-dom"
+
 export interface RepTopbarProps {
     onChangeYear:Function,
     year: number
 }
+
 export const RepTopbar: React.FC<RepTopbarProps> = ({onChangeYear, year}) => {
     const { selectedKey } = useMenu();
-    //TODO liste d'objet avec "key" et "libel" pour générer les Items
+    const items = [
+        {path:'/rep/deee', label:"D3E"},
+        {path:'/rep/pa', label:"Piles & Accu"},
+        {path:'/rep', label:"Vrac"},
+    ]
+    
     return (
         <Space direction="vertical" style={{ display: 'flex' }}>
             <Alert
@@ -22,15 +29,11 @@ export const RepTopbar: React.FC<RepTopbarProps> = ({onChangeYear, year}) => {
                   options={Array.from({ length: 2021 - 2018 + 1 }, (_, i) => 2018 + i).map((i) => ({label:i, value:i}) ).reverse()} />
 
                 <Menu mode="horizontal" selectedKeys={[selectedKey]}>
-                    <Menu.Item key="/rep"> 
-                        <NavLink to="/rep" className="nav-text">Vrac</NavLink>
+                    {items.map((item) =>
+                    <Menu.Item key={item.path}> 
+                        <NavLink to={item.path} className="nav-text">{item.label}</NavLink>
                     </Menu.Item>
-                    <Menu.Item key="/rep/deee">
-                        <NavLink to="/rep/deee" className="nav-text">D3E</NavLink>
-                    </Menu.Item>
-                    <Menu.Item key="/rep/pa">
-                        <NavLink to="/rep/pa" className="nav-text">Piles & Accu</NavLink>
-                    </Menu.Item>
+                    )}
                 </Menu>
 
             </Card>
