f3c72118055d77bbcd721e9fa4c1077e52a768f1
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-03-07 11:28:54 +0100

    side menu

--- src/components/rep_topbar/index.tsx
+++ src/components/rep_topbar/index.tsx
@@ -1,6 +1,6 @@
 import { useMenu } from "@refinedev/core";
 import { Alert, Card, Menu, Select, Space } from "antd"
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export interface RepTopbarProps {
     onChangeYear:Function,
@@ -8,17 +8,6 @@ export interface RepTopbarProps {
 }
 
 export const RepTopbar: React.FC<RepTopbarProps> = ({onChangeYear, year}) => {
-    const { selectedKey } = useMenu();
-    const items = [
-        {path:'/rep/deee', label:"D3E"},
-        {path:'/rep/pa', label:"Piles & Accu"},
-        {path:'/rep/pchim', label:"Produits chimiques"},
-        {path:'/rep/tlc', label:"Textiles"},
-        {path:'/rep/mnu', label:"Médicaments"},
-        {path:'/rep/disp_med', label:"DASRI"},
-        {path:'/rep', label:"Vrac"},
-    ]
-    
     return (
         <Space direction="vertical" style={{ display: 'flex' }}>
             <Alert
@@ -31,15 +20,6 @@ export const RepTopbar: React.FC<RepTopbarProps> = ({onChangeYear, year}) => {
             <Card>
                 <Select onChange={(e) => e ? onChangeYear(e) : undefined } defaultValue={year} value={year} 
                   options={Array.from({ length: 2021 - 2018 + 1 }, (_, i) => 2018 + i).map((i) => ({label:i, value:i}) ).reverse()} />
-
-                <Menu mode="horizontal" selectedKeys={[selectedKey]}>
-                    {items.map((item) =>
-                    <Menu.Item key={item.path}> 
-                        <NavLink to={item.path} className="nav-text">{item.label}</NavLink>
-                    </Menu.Item>
-                    )}
-                </Menu>
-
             </Card>
         </Space>
     )
--- src/layout/sider.tsx
+++ src/layout/sider.tsx
@@ -1,5 +1,5 @@
-import { ApiOutlined } from "@ant-design/icons";
-import { ThemedSiderV2, ThemedTitleV2 } from "@refinedev/antd";
+import { ApiOutlined, CloseSquareOutlined, FireOutlined, HomeOutlined, RollbackOutlined } from "@ant-design/icons";
+import { Sider, ThemedSiderV2, ThemedTitleV2 } from "@refinedev/antd";
 
 export const AppTitle: React.FC = () => {
 
@@ -11,7 +11,81 @@ export const AppTitle: React.FC = () => {
 export const AppSider: React.FC = () => {
     return (
         <>
-            <ThemedSiderV2 Title={() => <AppTitle/> } />
+            <CustomSider Title={() => <AppTitle/> } />
         </>
+    )
+}
+
+/*** WIP ***/
+
+import React, { CSSProperties } from "react";
+import {
+  useTranslate,
+  useLogout,
+  useTitle,
+  CanAccess,
+  ITreeMenu,
+  useIsExistAuthentication,
+  useRouterContext,
+  useMenu,
+  useRefineContext,
+  useLink,
+  useRouterType,
+  useActiveAuthProvider,
+  pickNotDeprecated,
+  useWarnAboutChange,
+} from "@refinedev/core";
+import { useThemedLayoutContext } from "@refinedev/antd";
+import {
+  DashboardOutlined,
+  LogoutOutlined,
+  UnorderedListOutlined,
+  BarsOutlined,
+  LeftOutlined,
+  RightOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, Grid, Drawer, Button, theme } from "antd";
+import type { RefineThemedLayoutV2SiderProps } from "@refinedev/antd";
+import { NavLink } from "react-router-dom";
+
+const drawerButtonStyles: CSSProperties = {
+  borderTopLeftRadius: 0,
+  borderBottomLeftRadius: 0,
+  position: "fixed",
+  top: 64,
+  zIndex: 999,
+};
+
+export const CustomSider: React.FC<RefineThemedLayoutV2SiderProps> = ({}) => {
+    const { token } = theme.useToken();
+
+    const { selectedKey } = useMenu();
+
+    const items =[
+        {key:'/dma', label:<NavLink to='/dma'><HomeOutlined /> DMA</NavLink>},
+        {key:'#/rep', label:<><RollbackOutlined /> REP</>, children:[
+            {key:'/rep/deee', label:<NavLink to='/rep/deee'>D3E</NavLink>},
+            {key:'/rep/pa', label:<NavLink to='/rep/pa'>Piles et Accu</NavLink>},
+            {key:'/rep/pchim', label:<NavLink to='/rep/pchim'>Produits chimiques</NavLink>},
+            {key:'/rep/tlc', label:<NavLink to='/rep/tlc'>Textiles</NavLink>},
+            {key:'/rep/mnu', label:<NavLink to='/rep/mnu'>Médicaments</NavLink>},
+            {key:'/rep/disp_med', label:<NavLink to='/rep/disp_med'>DASRI</NavLink>},
+            {key:'/rep', label:<NavLink to='/rep/'>Vrac</NavLink>},
+        ]},
+        {key:'/isdnd', disabled:true, label:<NavLink to='#'><CloseSquareOutlined /> Enfouissement</NavLink>},
+        {key:'/cve', disabled:true, label:<NavLink to='#'><FireOutlined /> Incinération</NavLink>},
+    ]
+
+    return (
+        <Layout.Sider style={{
+            height: "100vh",
+            backgroundColor: token.colorBgContainer,
+            borderRight: `1px solid ${token.colorBgElevated}`,
+        }}>
+            <ThemedTitleV2 collapsed={false} icon={<ApiOutlined/>} text="Odema"/>
+            <Menu items={items} selectedKeys={[selectedKey]} mode="inline">
+                
+            </Menu>
+        </Layout.Sider>
     )
 }
