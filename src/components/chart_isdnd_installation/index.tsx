50d9bc9f04c4683676fc15e22c0cf51d60024337
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-03-08 14:57:59 +0100

    chart isdnd

---
+++ src/components/chart_isdnd_installation/index.tsx
@@ -0,0 +1,63 @@
+import { BaseRecord } from "@refinedev/core"
+import alasql from "alasql";
+import { BarSeriesOption, EChartsOption } from "echarts";
+import ReactECharts from 'echarts-for-react'; 
+
+export interface IChartEvolutionISDND {
+    data : BaseRecord[]
+    aiot: string
+}
+export const ChartEvolutionISDND: React.FC<IChartEvolutionISDND> = ({ data, aiot }) => {
+
+
+    const data_chart = data
+    .filter((e) => e.aiot == aiot)
+    .map((e) => ({ serie_name: e.name, value: e.tonnage, category: e.annee }))
+    .sort((a, b) => a.category - b.category)
+
+    const axie_category = [...new Set(data_chart.map(item => item.category))]
+    .map((e) => ({
+        value: e
+    }));
+
+    const myseries: BarSeriesOption[] = alasql(`
+    SELECT d.[serie_name] AS name, ARRAY(d.[value]) AS data
+    FROM ? d
+    GROUP BY d.[serie_name]
+`, [data_chart]).map((e: BaseRecord) => (
+        {
+            name:e.name,
+            data:e.data,
+            type: 'bar',
+            stack: 'stack1',
+            tooltip:{
+                show:true,
+                valueFormatter: (value:number) => (`${Math.round(Number(value)).toLocaleString()} t` )
+            }
+        }
+    ))
+
+    const option: EChartsOption ={
+        series:myseries,
+        legend: {top:'top', show:true},
+        tooltip: {
+            trigger: 'item'
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: axie_category
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+    }
+
+    return (
+        <ReactECharts
+        option={option} style={{ height: "450px" }} />
+    )
+}
--- src/components/pages/enfouissement.tsx
+++ src/components/pages/enfouissement.tsx
@@ -1,7 +1,36 @@
-import { IResourceComponentsProps } from "@refinedev/core"
-import { Row, Col, Alert } from "antd"
+import { BaseRecord, IResourceComponentsProps, useList } from "@refinedev/core"
+import { Row, Col, Alert, Select, Card } from "antd"
+import {
+    useQuery,
+  } from "@tanstack/react-query";
+import axios from "axios";
+import { ChartEvolutionISDND } from "../chart_isdnd_installation";
+import { BaseOptionType } from "antd/es/select";
+import alasql from "alasql";
+import { useState } from "react";
+
+
 
 export const EnfouissementPage: React.FC<IResourceComponentsProps> = () => {
+
+    const [aiot, setAiot] = useState('0007003529')
+
+    const {data:data_isdnd} = useQuery({ // A remplacer par appel API WFS
+        queryKey: ['repoData'],
+        queryFn: () =>
+          axios
+            .get('/data/isdnd_tonnage_annee.json')
+            .then((res) => res.data),
+    })
+
+
+
+    const select_options:BaseOptionType[] = data_isdnd ? alasql(`
+        SELECT DISTINCT aiot AS [value], name AS label
+        FROM ?
+    `, [data_isdnd]).map((e:BaseRecord) => ({value:e.value, label:`${e.label} (${e.value})`})) : undefined
+
+    console.log(select_options)
 
     return (<>
       <Row gutter={[16, 16]}>
@@ -13,6 +42,22 @@ export const EnfouissementPage: React.FC<IResourceComponentsProps> = () => {
                         showIcon
                     />
                 </Col>
+                <Col span={12}>
+
+               { data_isdnd ? 
+                     <Card>
+                     <Select showSearch
+                     optionFilterProp="label" 
+                         defaultValue={aiot}
+                         onSelect={(e) => setAiot(e)}
+                        options={select_options} 
+                        style={{width:'100%'}}/>
+                        
+                    <ChartEvolutionISDND data={data_isdnd} aiot={aiot}></ChartEvolutionISDND> 
+                    </Card>
+                : <small>Chargement</small> }
+
+               </Col>
         </Row>
     </>)
 }
