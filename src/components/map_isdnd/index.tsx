6cadf955c95716417cbc8c74344cdc006cb2fa12
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-03-13 15:03:52 +0100

    conditionnal color map

--- src/components/map_isdnd/index.tsx
+++ src/components/map_isdnd/index.tsx
@@ -18,18 +18,14 @@ export const MapIsdnd: React.FC<IMapProps> = ({ data, aiot }) => {
   const year = 2022
   //const style = 'https://demotiles.maplibre.org/style.json';
 
-  const aiot_center = data.find((e) => e.aiot == aiot)
-  const center:LngLatLike = aiot_center ? [aiot_center.lng, aiot_center.lat] : [3,25] // ??
-
-  console.log(BaseRecordToGeojsonPoint({data:data, y:'lng',x:'lat'}))
-
 
 const source_raster:SourceProps = //Ressource commune (fond de plan)
   {
     'type': 'raster',
     'tiles': [
-        'https://www.geo2france.fr/geoserver/geo2france/ows/?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.3.0&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=ortho_regionale_2018_rvb'
-    ],
+        //'https://www.geo2france.fr/geoserver/geo2france/ows/?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.3.0&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=ortho_regionale_2018_rvb'
+        'https://osm.geo2france.fr/mapcache/?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=false&LAYERS=grey&TILED=true&WIDTH=256&HEIGHT=256&SRS=EPSG%3A3857&STYLES=&BBOX={bbox-epsg-3857}'
+      ],
     'tileSize': 256
     }
 
@@ -57,7 +53,9 @@ const layer_raster:LayerProps = {
           [1000000, 25]
         ]
       },  
-      'circle-color': '#3887be'
+      'circle-color': ['case', ['==', ['get', 'aiot'], aiot] ,'#3887be', "#828282"],
+      'circle-opacity': ['case', ['==', ['get', 'aiot'], aiot] ,0.8, 0.5]
+
     }
   }
 
@@ -76,25 +74,19 @@ const layer_raster:LayerProps = {
       'circle-color': 'black',
       "circle-opacity":0,
       "circle-stroke-width":2,
-      "circle-stroke-color":"#ff0000",
+      "circle-stroke-color":['case', ['==', ['get', 'aiot'], aiot] ,'#f00', "#828282"]
     }
   }
-
-  useEffect(() => {
-    if (mapRef.current){
-        mapRef.current.flyTo({center:center, speed: 0.8, zoom:7}) //TODO : Highlight installation, cf https://maplibre.org/maplibre-gl-js/docs/examples/hover-styles/
-    }
-  }, [aiot] )
 
   return (
     <Map
       ref={mapRef}
       initialViewState={{
-        latitude: center[1],
-        longitude: center[0],
+        latitude: 49.96462,
+        longitude: 2.820399,
         zoom: zoom
       }}
-      style={{ width: 800, height: 600 }}
+      style={{ width: '100%', height: 600 }}
       //mapStyle={style}
       attributionControl={false}
     >
