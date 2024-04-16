cc56962d28dcf5c727361639239f5c51bcefa354
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-04-16 10:12:47 +0200

    edit children size

--- src/components/dashboard_element/index.tsx
+++ src/components/dashboard_element/index.tsx
@@ -1,5 +1,6 @@
 import { FullscreenOutlined } from "@ant-design/icons"
 import { Card, Button, Modal } from "antd"
+import React from "react";
 
 //TODO integrer le composant loading container
 
@@ -16,13 +17,22 @@ export interface IDashboardElementProps{
  * @returns 
  */
 export const DashboardElement:React.FC<IDashboardElementProps> = ({children, modalIsOpen: isModalOpen,setModalIsOpen: setModalIsOpen, title}) => {
+    
+    const modifiedChildren = React.Children.map(children, (child, index) => {
+        // Vérifiez si c'est le deuxième enfant
+        if (index === 0 && React.isValidElement(child)) { //Détecter ici s'il s'agit d'un graphique, ou d'une carte (sur quelle base ? chartRef ?)
+            return React.cloneElement(child, { ...child.props?.style, style: {height:'80vh'} }); 
+        }
+        return child;
+    });
+    
     return(
         <Card title={title}>
             <Button onClick={(e) => setModalIsOpen(true)}><FullscreenOutlined /></Button>
             {children}
             <Modal title={title} open={isModalOpen} onCancel={(e) => setModalIsOpen(false)} onOk={(e) => setModalIsOpen(false)} footer={null}
             wrapClassName="modal-fullscreen">
-            {children}
+            {modifiedChildren}
             </Modal>
         </Card>
     )
