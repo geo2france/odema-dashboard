05b5d58dc462d6a3946de7bc9f0878b2741c38ff
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-04-24 09:54:13 +0200

    auto ajust ymin

--- src/components/chart_target_evolution/index.tsx
+++ src/components/chart_target_evolution/index.tsx
@@ -3,6 +3,7 @@ import ReactECharts from 'echarts-for-react';
 import { EChartsOption, LineSeriesOption } from 'echarts';
 import { CSSProperties, useRef } from 'react';
 import { BaseRecord } from '@refinedev/core';
+import alasql from 'alasql';
 
 
 interface IChartTargetEvolutionProps {
@@ -16,6 +17,15 @@ export const ChartTargetEvolution: React.FC<IChartTargetEvolutionProps> = ( {dat
 
     const target_data = [ [data[0].ref_date, data[0].ref_value], [data[0].due_date, data[0].target]]
 
+    // Calcule min et max pour définir les bornes de l'axe Y
+    const offset_coef = 0.5
+    const MinMax = alasql(` SELECT 
+      MIN(MIN([value], [ref_value], [target])) as min,
+      MAX(MAX([value], [ref_value], [target])) as max
+    FROM ?
+    `, [data]).map((e:BaseRecord) => ({...e, offset:(e.max - e.min)*offset_coef}))[0]
+
+    
     const chartRef = useRef<any>()
 
     const serie: LineSeriesOption = {
@@ -40,6 +50,7 @@ export const ChartTargetEvolution: React.FC<IChartTargetEvolutionProps> = ( {dat
       yAxis: [
         {
           type: "value",
+          min:Math.ceil(MinMax.min - MinMax.offset)
         },
       ],
     };
