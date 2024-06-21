2325f0e6132eee1766276b16beef5c4a61a83604
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-06-21 12:05:18 +0200

    move baserecordtogeojson

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
-import { BaseRecord } from "@refinedev/core";
-import { FeatureCollection, Feature } from "geojson";
 import { useSearchParams } from "react-router-dom";
 
 export { chartBusinessProps} from "./nomenclature";
@@ -75,34 +73,3 @@ export const default_app_palette = [
     "#ea7ccc"
 ]
 
-
-export interface IBaseRecordToGeojsonPoint{
-    data : BaseRecord[],
-    x : string,
-    y : string,
-    crs_name? :string
-}
-
-export interface FeatureCollection_crs extends FeatureCollection {crs?:any};
-
-export const BaseRecordToGeojsonPoint = ({data, x, y, crs_name}:IBaseRecordToGeojsonPoint) => {
-
-    //Tester maplibre avec CRS != 4326
-    const features:Feature[] = data.map((e:BaseRecord) => 
-       ( {
-        type:"Feature",
-        properties:{...e},
-        geometry:{
-            type:"Point",
-            coordinates:[e[y], e[x]]
-         }
-       }
-    ))
-
-    const geojson:FeatureCollection_crs = {"type": "FeatureCollection", features:features}
-
-    if (crs_name){
-        geojson.crs={type:"name", properties:{"name":crs_name}} //no longer supported in geoJSON ? https://github.com/DefinitelyTyped/DefinitelyTyped/issues/21794
-    }
-    return geojson
-}
