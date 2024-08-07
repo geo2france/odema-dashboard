a170c7cba14d1e47774b2d593cefefc75a80540a
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2024-08-07 09:57:37 +0200

    home

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,8 +1,60 @@
 import { Card, Col, Divider, Row, Typography } from "antd"
-import { FaCreativeCommons, FaCreativeCommonsBy, FaEnvelope, FaGithub } from "react-icons/fa";
+import { FaCreativeCommons, FaCreativeCommonsBy, FaDatabase, FaEnvelope, FaGithub, FaMapMarked } from "react-icons/fa";
+import { BsPersonWorkspace } from "react-icons/bs";
+import { List } from 'antd';
+import { FaHome } from "react-icons/fa";
 import Odema from "/img/logo_odema.png";
+import { ReactNode } from "react";
 
 const { Text, Link, Title } = Typography;
+
+
+const ListRelated:React.FC = () => {
+  const data:{description?:string, url?:string, title:string, picto?:ReactNode}[] = [
+    {
+      title: "Site web de l'Odema",
+      description:"Site public de l'observatoire",
+      url:"https://www.cerdd.org/L-Odema-Observatoire-dechets-matieres-des-Hauts-de-France",
+      picto:<FaHome/>
+    },
+    {
+      title: "Accès aux données",
+      description:"Datahub, catalogue de données Géo2France",
+      url:"https://www.geo2france.fr/datahub/search?q=odema",
+      picto:<FaDatabase />
+    },
+    {
+      title: "Cartographie intéractive",
+      description: "Visionneuse cartographique mViewer",
+      url:"https://www.geo2france.fr/mviewer/?config=apps/obs_dechet.xml",
+      picto:<FaMapMarked />
+    },
+    {
+      title: "Groupe projet et accès aux données sensibles",
+      description:"Accès membres de l'Odema",
+      picto:<BsPersonWorkspace />
+    }
+  ]
+
+  return (
+    <List 
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={ (item, index) => (
+        <List.Item>
+          <List.Item.Meta 
+            title={<a href={item.url}>{item.title}</a>}
+            description={item.description}
+            avatar={item.picto}
+            style={{marginLeft:10}}
+          />
+        </List.Item>
+
+    )}
+    />
+
+  )
+}
 
 export const HomePage:React.FC = () => {
     return (
@@ -18,76 +70,58 @@ export const HomePage:React.FC = () => {
               </Col>
               <Col md={18}>
                 <Title level={1}>Tableaux de bords de l'Odema</Title>
-
                 <p>
-                  Vous trouverez ici les <Text strong>tableaux de bord thématiques</Text> élaborés
-                  par l'Observatoire déchets-matières des Hauts-de-France
-                  (Odema).
+                  Vous trouverez ici les{" "}
+                  <Text strong>tableaux de bord thématiques</Text> élaborés par
+                  l'Observatoire déchets-matières des Hauts-de-France (Odema).
                 </p>
                 <p>
                   Pour toute sollicitation, contactez-nous à{" "}
                   <Link href="mailto:odema@cerdd.org">
                     odema@cerdd.org <FaEnvelope />
-                  </Link>
-                  .
+                  </Link>.
                 </p>
               </Col>
             </Row>
-
-            <Divider />
-
-            <Title level={2}>A voir aussi</Title>
-
-            <ul>
-              <li>
-                <Link href="https://www.cerdd.org/L-Odema-Observatoire-dechets-matieres-des-Hauts-de-France">
-                  Site web de l'Odema
-                </Link>
-              </li>
-              <li>
-                <Link href="https://www.geo2france.fr/datahub/search?q=odema">
-                  Accès aux données
-                </Link>
-              </li>
-              <li>
-                <Link href="https://www.geo2france.fr/mviewer/?config=apps/obs_dechet.xml">
-                  Cartographie intéractive
-                </Link>
-              </li>
-              <li>
-                <Text disabled>Groupe projet et accès données sensibles</Text>
-              </li>
-            </ul>
-            <Divider />
-            <Title level={2}>A propos</Title>
-            <p>
-              Le code source de ce tableau de bord est libre et peut-être
-              consulter sur le{" "}
-              <Link href="https://github.com/geo2france/odema-dashboard">
-                {" "}
-                dépot de code <FaGithub />
-              </Link>
-              .
-            </p>
-            <p>
-              Cette application est alimentée simultanément par des données
-              hébergées par{" "}
-              <Link href="https://www.geo2france.fr/">Géo2France</Link> et par
-              l'<Link href="https://data.ademe.fr/">Ademe</Link>. La source est
-              indiquée sous chaque graphique. La plupart des données sont sous
-              licence libre et peuvent être réutilisées.
-            </p>
-            <p>
-              Sauf mention contraire, les graphiques présentés ici sont sous
-              licence{" "}
-              <Link href="https://creativecommons.org/licenses/by/2.0/fr/deed.fr">
-                {" "}
-                CC BY <FaCreativeCommons /> <FaCreativeCommonsBy />
-              </Link>{" "}
-              et peuvent donc également être librement ré-utilisés sous réserve
-              de citation de l'auteur (Observatoire déchets-matières des
-              Hauts-de-France).{" "}
-            </p>
+            <Divider/>
+            <Row gutter={[16, 16]}>
+              <Col md={12} style={{ width: "100%" }}>
+                <Card title="A voir aussi">
+                  <ListRelated />
+                </Card>
+              </Col>
+              <Col md={12}>
+                <Card title="A propos" style={{ height: "100%" }}>
+                  <p>
+                    Le code source de ce tableau de bord est libre et peut-être
+                    consulter sur le{" "}
+                    <Link href="https://github.com/geo2france/odema-dashboard">
+                      dépot de code <FaGithub />
+                    </Link>
+                    .
+                  </p>
+                  <p>
+                    Cette application est alimentée simultanément par des
+                    données hébergées par{" "}
+                    <Link href="https://www.geo2france.fr/">Géo2France</Link> et
+                    par l'<Link href="https://data.ademe.fr/">Ademe</Link>. La
+                    source est indiquée sous chaque graphique. La plupart des
+                    données sont sous licence libre et peuvent être réutilisées.
+                  </p>
+                  <p>
+                    Sauf mention contraire, les graphiques présentés ici sont
+                    sous licence{" "}
+                    <Link href="https://creativecommons.org/licenses/by/2.0/fr/deed.fr">
+                      {" "}
+                      CC BY <FaCreativeCommons /> <FaCreativeCommonsBy />
+                    </Link>{" "}
+                    et peuvent donc également être librement ré-utilisés sous
+                    réserve de citation de l'auteur (Observatoire
+                    déchets-matières des Hauts-de-France).{" "}
+                  </p>
+                </Card>
+              </Col>
+            </Row>
           </Card>
         </Col>
       </Row>
