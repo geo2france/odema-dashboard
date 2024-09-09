0dd26c144559bd7f495e2378d1700cc9b8f0a9e9
MaximeGuillemin <73181691+Maxime-Guillemin@users.noreply.github.com>
2024-09-09 11:22:00 +0200

    Footer #9

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,6 +1,6 @@
 import { Layout, Divider } from "antd";
-import { CSSProperties } from "react";
-import { grey } from '@ant-design/colors';
+import { CSSProperties, useState } from "react";
+import { grey } from "@ant-design/colors";
 
 import Ademe from "/img/Logo_ADEME.svg";
 import Prefet from "/img/Préfet_de_la_région_Hauts-de-France.svg";
@@ -10,46 +10,86 @@ import Cerdd from "/img/Logo_cerdd.svg";
 import Geo2France from "/img/geo2france.svg";
 import Odema from "/img/logo_odema.png";
 
-export const AppFooter: React.FC = () => {
+import { UpOutlined, DownOutlined } from "@ant-design/icons"; // Utilisation d'icônes Ant Design pour la flèche
 
-  const style_img:CSSProperties = { 
-    height:'40px',
-    marginRight: '20px'
-  }
+export const AppFooter: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggleFooter = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const style_img: CSSProperties = {
+    height: "40px",
+    marginRight: "20px",
+  };
 
   const logos = [
-    {src : Ademe, name: "Ademe", url: "https://www.ademe.fr/"},
-    {src : Prefet, name: "Préfecture Hauts-de-France", url: "https://www.hauts-de-france.developpement-durable.gouv.fr/"},
-    {src : Region, name: "Région Hauts-de-France", url: "https://www.hautsdefrance.fr/"},
-    {src : Cerc, name: "CERC Hauts-de-France", url: "https://www.cerc-hautsdefrance.fr/"},
-    {src : Cerdd, name: "CERDD", url: "https://www.cerdd.org/"},
-    {src : Geo2France, name: "Géo2France", url: "https://www.geo2france.fr/"}
+    { src: Ademe, name: "Ademe", url: "https://www.ademe.fr/" },
+    {
+      src: Prefet,
+      name: "Préfecture Hauts-de-France",
+      url: "https://www.hauts-de-france.developpement-durable.gouv.fr/",
+    },
+    {
+      src: Region,
+      name: "Région Hauts-de-France",
+      url: "https://www.hautsdefrance.fr/",
+    },
+    {
+      src: Cerc,
+      name: "CERC Hauts-de-France",
+      url: "https://www.cerc-hautsdefrance.fr/",
+    },
+    { src: Cerdd, name: "CERDD", url: "https://www.cerdd.org/" },
+    { src: Geo2France, name: "Géo2France", url: "https://www.geo2france.fr/" },
+  ];
+  return (
+    <Layout.Footer
+      style={{
+        textAlign: "center",
+        color: "#fff",
+        backgroundColor: "#fff",
+        bottom: isVisible ? "0" : "-100px", 
+        position: "sticky",
 
-  ]
-  return(
-      <Layout.Footer
-          style={{
-            textAlign: "center",
-            color: "#fff",
-            backgroundColor: "#fff",
-            position:"sticky",
-            bottom:0,
-            right:"0px",
-            width:"100%"
-          }}
-        >
-            <div>
-            <a href="/">
-              <img style={style_img} src={Odema} alt="Odema"/>
-            </a>
-            <Divider type="vertical" style={{ height: "30px", backgroundColor: grey[2] }} />
-            { logos.map((l) => 
-              <a href={l.url} key={l.name}> 
-                <img style={style_img}
-                  src={l.src} alt={l.name}/>
-                </a>
-            )}
-          </div>
-        </Layout.Footer>
-  )
-}
+        right: "0px",
+        transition: "bottom 0.5s", 
+        width: "100%",
+      }}
+    >
+      <div>
+        <a href="/">
+          <img style={style_img} src={Odema} alt="Odema" />
+        </a>
+        <Divider
+          type="vertical"
+          style={{ height: "30px", backgroundColor: grey[2] }}
+        />
+        {logos.map((l) => (
+          <a href={l.url} key={l.name}>
+            <img style={style_img} src={l.src} alt={l.name} />
+          </a>
+        ))}
+      </div>
+
+      {/* Bouton de contrôle pour afficher ou cacher le footer */}
+      <div
+        style={{
+          position: "fixed",
+          bottom: isVisible ? "100px" : "10px",
+          right: "10px",
+          cursor: "pointer",
+          zIndex: 1001,
+        }}
+        onClick={toggleFooter}
+      >
+        {isVisible ? (
+          <DownOutlined style={{ fontSize: "20px", color: "#000" }} />
+        ) : (
+          <UpOutlined style={{ fontSize: "20px", color: "#000" }} />
+        )}
+      </div>
+    </Layout.Footer>
+  );
+};
