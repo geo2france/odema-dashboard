fcf3d790fda7aa9c2829482aac4b7b414b207bd6
Maxime-Guillemin <max.guillemin@free.fr>
2024-11-05 11:23:43 +0100

    Ajout d'un footer collapse

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -10,21 +10,21 @@ import Cerdd from "/img/Logo_cerdd.svg";
 import Geo2France from "/img/geo2france.svg";
 import Odema from "/img/logo_odema.png";
 
-import { UpOutlined, DownOutlined } from "@ant-design/icons"; 
+import { UpOutlined, DownOutlined } from "@ant-design/icons";
 
 export const AppFooter: React.FC = () => {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
-  const toggleFooter = () => {
-    setIsVisible(!isVisible);
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
   };
 
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth < 768) {
-        setIsVisible(false);
+        setIsCollapsed(true);
       } else {
-        setIsVisible(true);
+        setIsCollapsed(false);
       }
     };
     window.addEventListener("resize", handleResize);
@@ -36,7 +36,7 @@ export const AppFooter: React.FC = () => {
   }, []);
 
   const style_img: CSSProperties = {
-    height: "40px",
+    height: "60px",
     marginRight: "20px",
   };
 
@@ -67,15 +67,31 @@ export const AppFooter: React.FC = () => {
         textAlign: "center",
         color: "#fff",
         backgroundColor: "#fff",
-        bottom: isVisible ? "0" : "-300px", 
-        position: "sticky", 
-        right: "0px",
-        transition: "bottom 0.5s ease-in-out", 
+        bottom: "0",
+        position: "sticky",
+        right: "0",
         width: "100%",
-       
+        height: isCollapsed ? "0px" : "150px", 
+        transition: "height 0.5s ease-in-out",
+        overflow: "hidden",
+        borderTop: "1px solid #ccc", 
       }}
     >
-      <div>
+      {/* Texte affiché uniquement lorsque le footer est rétracté */}
+      {isCollapsed && (
+        <div style={{
+          color: "#000",
+          fontSize: "14px",
+          position: "absolute", 
+          top: "15px", 
+          left: "50%", 
+        }}>
+          © ODEMA
+        </div>
+      )}
+
+      {/* Logos et contenu du footer affichés lorsque déplié */}
+      <div style={{ display: isCollapsed ? "none" : "block", padding: "10px 0"}}>
         <a href="/">
           <img style={style_img} src={Odema} alt="Odema" />
         </a>
@@ -90,21 +106,27 @@ export const AppFooter: React.FC = () => {
         ))}
       </div>
 
-      {/* Bouton de contrôle pour afficher ou cacher le footer */}
+      {/* Bouton carré de contrôle pour afficher ou cacher le footer */}
       <div
         style={{
-          position: "fixed",
-          bottom: "10px", 
+          position: "absolute",
+          bottom: "5px",
           right: "10px",
           cursor: "pointer",
           zIndex: 1001,
+          backgroundColor: "#dead8f",
+          padding: "5px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "4px",
         }}
-        onClick={toggleFooter}
+        onClick={toggleCollapse}
       >
-        {isVisible ? (
-          <DownOutlined style={{ fontSize: "20px", color: "#000" }} />
+        {isCollapsed ? (
+          <UpOutlined style={{ fontSize: "16px", color: "#fff" }} />
         ) : (
-          <UpOutlined style={{ fontSize: "20px", color: "#000" }} />
+          <DownOutlined style={{ fontSize: "16px", color: "#fff" }} />
         )}
       </div>
     </Layout.Footer>
