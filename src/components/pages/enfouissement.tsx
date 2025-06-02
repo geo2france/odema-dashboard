d0c0120c9ca0a84d1b8fa897f693e47554bcc5f4
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2025-06-02 14:48:25 +0200

    chiffres-clés isdnd

--- src/components/pages/enfouissement.tsx
+++ src/components/pages/enfouissement.tsx
@@ -1,8 +1,8 @@
 import { CSSProperties, useState } from "react";
 import { Row, Col, Drawer, Tooltip, Select, Flex, Typography } from "antd"
 import alasql from "alasql";
-
-import { Control, DashboardElement, NextPrevSelect, SimpleRecord, useApi, useSearchParamsState } from "api-dashboard";
+import * as aq from 'arquero';
+import { Control, DashboardElement, KeyFigure, NextPrevSelect, SimpleRecord, useApi, useSearchParamsState } from "api-dashboard";
 
 import { ChartEvolutionISDND } from "../chart_isdnd_installation";
 import { ChartRaceBarISDND } from "../chart_isdnd_racebar";
@@ -12,6 +12,10 @@ import { ChartIsdndGlobal } from "../chart_isdnd_global";
 import { HistoryOutlined } from "@ant-design/icons";
 import { ChartDonutIsdndCapacite } from "../chat_donut_isdnd_capacite";
 import { geo2franceProvider } from "../../App";
+import { FaLocationPin } from "react-icons/fa6";
+import { MdFrontLoader } from "react-icons/md";
+import { AiOutlineFall, AiOutlineRise } from "react-icons/ai";
+import { GiResize } from "react-icons/gi";
 
 
 export const EnfouissementPage: React.FC = () => {
@@ -34,6 +38,19 @@ export const EnfouissementPage: React.FC = () => {
             properties:['annee','aiot','name','tonnage','capacite','lat','lng','departement'] //Reduce size, keep only used fields
         }
     })
+    
+
+    const key_figures:SimpleRecord[] | undefined = data_isdnd?.data && aq.from(data_isdnd?.data)
+        .filter(d => d.tonnage > 0)
+        .groupby('annee')
+        .rollup({ 
+            tonnage: d => aq.op.sum(d.tonnage) , 
+            n_installation : aq.op.count() ,
+            capacite : d => aq.op.sum(d.capacite) 
+        }).objects()
+
+    const current_key_figures = key_figures?.find((e) => e.annee==Number(year))
+
 
     const select_options = data_isdnd && (alasql(`
         SELECT DISTINCT aiot AS [value], name AS label
@@ -83,20 +100,66 @@ export const EnfouissementPage: React.FC = () => {
                   </DashboardElement>
                </Col>
 
+               <Col xl={12} xs={24}>
+                   <DashboardElement isFetching={isFetchingIsdnd || isFetchingCapacite} title={`Chiffres-clé ${year}`}  attributions={[{name : 'GT ISDND', url:'https://www.geo2france.fr/datahub/dataset/1a1480b4-8c8b-492d-9cd0-a91b49576017'},{name: 'Odema'}]}>
+                    <Row gutter={[8,8]} style={{margin:8}}>
+                    <Col xl={24/3} md={24/3} xs={24}> 
+                        <KeyFigure 
+                         value={current_key_figures?.n_installation}
+                         digits={0}
+                         name="Installations en service"
+                         icon={<FaLocationPin />}
+                         description={`Installations ayant enfoui des déchets en ${year}`}
+                        />
+                    </Col>
+                    <Col xl={24/3} md={24/3} xs={24}> 
+                        <KeyFigure 
+                         value={current_key_figures?.tonnage}
+                         unit="T"
+                         digits={0}
+                         name="Quanités enfouies"
+                         icon={<MdFrontLoader />}
+                        />
+                    </Col>
+                    <Col xl={24/3} md={24/3} xs={24}> 
+                        <KeyFigure 
+                         value={ ( 100 * (current_key_figures?.tonnage - key_figures?.find((e) => e.annee==Number(2010))?.tonnage )
+                                    / key_figures?.find((e) => e.annee==Number(2010))?.tonnage )  }
+                         unit="%"
+                         digits={2}
+                         name={`Evolution 2010-${year}`}
+                         description="Evolution des quantités enfouies depuis l'année de référence 2010"
+                         icon={(current_key_figures?.tonnage - key_figures?.find((e) => e.annee==Number(2010))?.tonnage ) > 0 ? <AiOutlineRise /> : <AiOutlineFall />}
+                        />
+                    </Col>
+
+                    <Col xl={24/3} md={24/3} xs={24}> 
+                        <KeyFigure 
+                         value={current_key_figures?.capacite}
+                         unit="T"
+                         digits={0}
+                         name="Capacité réglementaire"
+                         icon={<GiResize />}
+                        />
+                    </Col>
+
+                    </Row>
+                   </DashboardElement>
+               </Col>
+
                 <Col xl={12} xs={24}>
                   <DashboardElement isFetching={isFetchingIsdnd} title={`Tonnage enfouis par installation en ${year}`} attributions={[{name : 'GT ISDND', url:'https://www.geo2france.fr/datahub/dataset/1a1480b4-8c8b-492d-9cd0-a91b49576017'},{name: 'Odema'}]}>
                     { data_isdnd && <ChartRaceBarISDND style={chartStyle} data={data_isdnd.data} year={year} aiot={aiot} onClick={(e:any) => setAiot(e.data.key)} /> }
                   </DashboardElement>  
                 </Col>
 
-               <Col xl={8} lg={12} xs={24}>
+               <Col xl={12} lg={12} xs={24}>
                  <DashboardElement isFetching={isFetchingIsdnd} title={`Repartition des capacités autorisées ${year}`} attributions={[{name : 'GT ISDND', url:'https://www.geo2france.fr/datahub/dataset/1a1480b4-8c8b-492d-9cd0-a91b49576017'},{name: 'Odema'}]}>
                   { data_isdnd &&  <ChartDonutIsdndCapacite style={chartStyle} data={data_isdnd.data} year={year} aiot={aiot} onClick={(e:any) => setAiot(e.data.aiot)} /> }
                  </DashboardElement> 
                 </Col>
 
-                <Col xl={8} lg={12} xs={24}>
-
+                <Col xl={12} lg={12} xs={24}>
 
                      <DashboardElement isFetching={isFetchingIsdnd || isFetchingCapacite} title={`Tonnage enfouis : ${data_isdnd?.data.find((e:SimpleRecord) => e.aiot == aiot)?.name}`} attributions={[{name : 'GT ISDND', url:'https://www.geo2france.fr/datahub/dataset/1a1480b4-8c8b-492d-9cd0-a91b49576017'},{name: 'Odema'}]}>
                      { data_isdnd &&  data_capacite &&  
@@ -116,7 +179,7 @@ export const EnfouissementPage: React.FC = () => {
 
                </Col>
 
-               <Col xl={8} lg={12} xs={24}>
+               <Col xl={12} lg={12} xs={24}>
                  <DashboardElement isFetching={isFetchingIsdnd} title={`Installations de stockage de déchets non dangereux (ISDND) ${year}`} >
                      { data_isdnd &&  <MapIsdnd style={{height:376}} data={data_isdnd.data} year={year} aiot={aiot} onClick={(e:any) => setAiot(e.aiot)} /> }
                  </DashboardElement>
