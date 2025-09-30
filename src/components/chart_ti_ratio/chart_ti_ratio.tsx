7c0aafa5287e4a0f356e268cf4fb72c52c4d0ce5
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2025-09-30 12:05:53 +0200

    chart title, remove console log

--- src/components/chart_ti_ratio/chart_ti_ratio.tsx
+++ src/components/chart_ti_ratio/chart_ti_ratio.tsx
@@ -1,39 +1,55 @@
-import { ChartEcharts, useDataset } from "@geo2france/api-dashboard/dsl"
+import { ChartEcharts, useBlockConfig, useDataset } from "@geo2france/api-dashboard/dsl"
 import { EChartsOption } from "echarts"
-
 
 interface ChartTiRatioProps {
     dataset?: string
+    color?:string
+    dataKey:string
+    title?:string
 }
-export const ChartTiRatio: React.FC<ChartTiRatioProps> = ({dataset}) => {
+export const ChartTiRatio: React.FC<ChartTiRatioProps> = ({dataset, color, dataKey, title}) => {
     const data = useDataset(dataset)
     const chart_data = data?.data
-    console.log(data?.data)
+
+    const groups = ["Classique", "Incitative"]
+    useBlockConfig({
+        title:title,
+        dataExport: chart_data
+    })
     const option:EChartsOption = {
-         title: {
-                text: 'Scatter with Jittering'
-            },
             xAxis: {
                 type: 'category',
-                data: ['Classique', 'Incitative']
+                data: groups,
+                name:'Tarification',
+                jitter: 50,
+                jitterOverlap:false
             },
             yAxis: {
-                type: 'value'
+                type: 'value',
+                name: 'kg/hab'
+            },
+            tooltip : {
+                show:true,
+                trigger:"item"
             },
             series: [
                 {
-                name: 'Sleeping Hours',
                 type: 'scatter',
-                data : chart_data?.map((r) => [r.tarification, r.ratio]),
-                colorBy: 'data',
-                symbolSize: 5,
+                data : chart_data?.map((r) => [r.tarification, r[dataKey], r.name ]),
                 itemStyle: {
                     opacity: 0.4,
+                    color: color
+                    },
+                tooltip : {
+                    show:true,
+                    formatter:(p:any) => `${p.data?.[2]} - <b>${p.data?.[1]?.toLocaleString()} kg/hab</b>`
                 }
-                }
+                },
+
             ]
     }
     return (
+        // @ts-expect-error need update echarts
         <ChartEcharts option={option}/>
     )
 }
--- src/components/pages/dma_epci_home.tsx
+++ src/components/pages/dma_epci_home.tsx
@@ -1,4 +1,4 @@
-import { DashboardElement, DashboardPage, useApi } from "@geo2france/api-dashboard"
+import { useApi } from "@geo2france/api-dashboard"
 import { MapEPCIDMA } from "../map_epci_dma/mapEpciDma"
 import { geo2franceProvider } from "../../App"
 import { Dashboard, Dataset, Filter, Join, Transform } from "@geo2france/api-dashboard/dsl"
@@ -7,7 +7,7 @@ import { ChartTiRatio } from "../chart_ti_ratio/chart_ti_ratio"
 
 
 export const DmaPageEPCIHome: React.FC = () => {
-
+    const annee = 2023
     const dma_indic = useApi({
         dataProvider:geo2franceProvider,
         resource: "odema:destination_dma_epci_harmonise",
@@ -18,25 +18,13 @@ export const DmaPageEPCIHome: React.FC = () => {
 
     return  (
      <Dashboard debug>
-
-        <Dataset
-          id="epci"
-          type="wfs"
-          url="https://www.geo2france.fr/geoserver/odema/ows"
-          resource="odema:territoire_epci"
-        >
-            <Filter field="annee">2023</Filter>
-            <Filter field="population_collecte" operator="gt">0</Filter>
-        </Dataset>
-
         <Dataset
           id="epci_tonnage"
           type="wfs"
           url="https://www.geo2france.fr/geoserver/odema/ows"
           resource="odema:destination_dma_epci_harmonise"
         >
-            <Filter field="annee">2023</Filter>
-            <Filter field="type_dechet">{"Ordures ménagères résiduelles"}</Filter>
+            <Filter field="annee">{annee.toString()}</Filter>
         </Dataset>
 
         <Dataset
@@ -45,19 +33,24 @@ export const DmaPageEPCIHome: React.FC = () => {
           url="https://www.geo2france.fr/geoserver/odema/ows"
           resource="odema:population_tarification_ti_epci"
         >
-            <Filter field="annee">2023</Filter>
-             {/*<Join dataset="epci" joinKey={["epci_siren","siren"]} />*/}
+            <Filter field="annee">{annee.toString()}</Filter>
            <Join dataset="epci_tonnage" joinKey={["epci_siren", "siren_epci"]}></Join> 
             <Transform>{(data) => data.map((r) => ({
                 siren:r.epci_siren, 
                 name:r.epci_nom,
                  tarification:r.type_tarification,
-                 ratio: r.ratio_hab_pap}))}</Transform>
-           <Transform>Select [siren], [name], [tarification], sum([ratio]) as ratio FROM ? GROUP BY [siren], [name], [tarification]</Transform>
+                 type_dechet:r.type_dechet,
+                 ratio: r.ratio_hab_pap + r.ratio_hab_dechetterie}))}</Transform>
+           <Transform>Select [siren], [name], [tarification], 
+            sum(CASE WHEN [type_dechet]="Ordures ménagères résiduelles" THEN [ratio] END) as ratio_omr, 
+            sum(CASE WHEN [type_dechet]!="Ordures ménagères résiduelles" THEN [ratio] END) as ratio_cs
+             FROM ? GROUP BY [siren], [name], [tarification]</Transform>
         </Dataset>
         
-        <ChartTiRatio dataset="epci_ti" />
-            <MapEPCIDMA data={dma_indic.data?.data}/>
+        <ChartTiRatio dataset="epci_ti" dataKey="ratio_omr" color="grey" title="OMR collectées et type de tarification"/>
+        <ChartTiRatio dataset="epci_ti" dataKey="ratio_cs" color="orange" title="Autres collectes et type de tarification"/>
+
+        <MapEPCIDMA data={dma_indic.data?.data}/>
     </Dashboard>
     )
 
