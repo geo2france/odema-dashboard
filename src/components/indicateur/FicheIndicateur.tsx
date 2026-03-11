13265c0a10e624f1c921ef5a8fdef42bb4d9ae42
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2026-03-11 16:59:38 +0100

    code indent

--- src/components/indicateur/FicheIndicateur.tsx
+++ src/components/indicateur/FicheIndicateur.tsx
@@ -152,56 +152,59 @@ digits}) => {
         }}
         >
             <Flex justify="space-between">
-                    <Flex vertical align="center" justify="space-evenly" 
-                            style={{width:"100%", textAlign:"center", paddingTop:4, paddingBottom:4}}>
-                        <Flex justify="center" align="center" style={{width:"100%"}} gap={4}>
-                            <Icon icon="mdi:calendar" color={token.colorTextSecondary} />
-                            <Text >{ ANNEE }</Text>
-                        </Flex>
-                        <Flex align="center" justify="center" style={{width:"100%"}}>
-                            <Avatar
-                                size={32}
-                                icon={<Icon icon="iconoir:test-tube-solid"/>}
-                                style={{ backgroundColor: color, verticalAlign:'middle', margin:'0 8px'  }}
+                <Flex vertical align="center" justify="space-evenly" 
+                        style={{width:"100%", textAlign:"center", paddingTop:4, paddingBottom:4}}>
+
+                    <Flex justify="center" align="center" style={{width:"100%"}} gap={4}>
+                        <Icon icon="mdi:calendar" color={token.colorTextSecondary} />
+                        <Text >{ ANNEE }</Text>
+                    </Flex>
+
+                    <Flex align="center" justify="center" style={{width:"100%"}}>
+                        <Avatar
+                            size={32}
+                            icon={<Icon icon="iconoir:test-tube-solid"/>}
+                            style={{ backgroundColor: color, verticalAlign:'middle', margin:'0 8px'  }}
+                        />
+                        <span>
+                            <Text strong style={{fontSize:"180%", paddingRight:4}}>
+                                { current_value.toLocaleString(undefined, {maximumFractionDigits:digits}) }
+                            </Text> 
+                            <Text>{unit}</Text>
+                        </span>
+                    </Flex>
+                    
+                    {last_goal && 
+                    <div style={{width:"100%"}}>
+
+                        <Divider size="small" titlePlacement="start"> 
+                            <Text type="secondary">Objectif</Text> 
+                        </Divider>
+
+                        <span>
+                            <Icon icon="octicon:goal-16" fontSize={16} color={token.colorTextSecondary} /> 
+                            <Text type="secondary" italic> <strong>{goal_value} {unit}</strong> en {goal_year}</Text>
+                        </span>
+
+                        <Flex style={{width:"100%"}} justify="center">
+                            <Progress 
+                            type="line"
+                            steps={6}
+                            percent={ Math.round(percent * 100) }
+                            strokeColor={[token.colorError, token.colorError, 
+                                        token.colorWarning,token.colorWarning,
+                                        token.colorSuccess,token.colorSuccess]}
+                            showInfo={ false } 
                             />
-                            <span>
-                                <Text strong style={{fontSize:"180%", paddingRight:4}}>
-                                    { current_value.toLocaleString(undefined, {maximumFractionDigits:digits}) }
-                                </Text> 
-                                <Text>{unit}</Text>
-                            </span>
+                            { percent >=1 && 
+                            <Icon icon="lets-icons:check-fill" 
+                                color={token.colorSuccess}
+                                width={28}
+                                style={{ verticalAlign: "middle" }} /> }
                         </Flex>
-                            {last_goal && 
-                            <div style={{width:"100%"}}>
 
-                                <Divider size="small" titlePlacement="start"> 
-                                    <Text type="secondary">Objectif</Text> 
-                                </Divider>
-
-                                <span>
-                                    <Icon icon="octicon:goal-16" fontSize={16} color={token.colorTextSecondary} /> 
-                                    <Text type="secondary" italic> <strong>{goal_value} {unit}</strong> en {goal_year}</Text>
-                                </span>
-
-                                <Flex style={{width:"100%"}} justify="center">
-                                    <Progress 
-                                    type="line"
-                                    steps={6}
-                                    percent={ Math.round(percent * 100) }
-                                    strokeColor={[token.colorError, token.colorError, 
-                                                token.colorWarning,token.colorWarning,
-                                                token.colorSuccess,token.colorSuccess]}
-                                    showInfo={ false } 
-                                    />
-                                    { percent >=1 && 
-                                    <Icon icon="lets-icons:check-fill" 
-                                        color={token.colorSuccess}
-                                        width={28}
-                                        style={{ verticalAlign: "middle" }} /> }
-                                </Flex>
-
-                            </div> }
-                        </Flex>
+                    </div> }
+                </Flex>
 
                 { showChart && 
                 <div
