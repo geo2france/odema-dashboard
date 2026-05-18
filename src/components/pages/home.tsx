0fa5ce4e17397f661d8e3239273a30f1d92493d2
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2026-05-18 11:55:49 +0200

    link homepage

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,63 +1,15 @@
-import { Card, Col, Row, Typography } from "antd"
-import { FaCreativeCommons, FaCreativeCommonsBy, FaDatabase, FaEnvelope, FaGithub, FaMapMarked } from "react-icons/fa";
-//import { BsPersonWorkspace } from "react-icons/bs";
-import { List } from 'antd';
-import { FaHome } from "react-icons/fa";
+import { Card, Col, Flex, Row, theme, Typography } from "antd"
+import { FaCreativeCommons, FaCreativeCommonsBy, FaEnvelope, FaGithub } from "react-icons/fa";
 import Odema from "/img/logo_odema.png";
-import { ReactNode } from "react";
 import { PageProps } from "@geo2france/api-dashboard";
+import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom"
+const { Text, Title } = Typography;
+const { useToken } = theme;
 
-const { Text, Link, Title } = Typography;
-
-
-const ListRelated:React.FC = () => {
-  const data:{description?:string, url?:string, title:string, picto?:ReactNode}[] = [
-    {
-      title: "Site web de l'Odema",
-      description:"Site public de l'observatoire",
-      url:"https://odema-hautsdefrance.org",
-      picto:<FaHome/>
-    },
-    {
-      title: "Accès aux données",
-      description:"Datahub, catalogue de données Géo2France",
-      url:"https://www.geo2france.fr/datahub/search?q=odema",
-      picto:<FaDatabase />
-    },
-    {
-      title: "Cartographie interactive",
-      description: "Visionneuse cartographique mViewer",
-      url:"https://www.geo2france.fr/mviewer/?config=apps/odema/obs_dechet.xml",
-      picto:<FaMapMarked />
-    },
-   /* {
-      title: "Groupe projet et accès aux données sensibles",
-      description:"Accès membres de l'Odema",
-      picto:<BsPersonWorkspace />
-    }*/
-  ]
-
-  return (
-    <List 
-      itemLayout="horizontal"
-      dataSource={data}
-      renderItem={ (item, _index) => (
-        <List.Item>
-          <List.Item.Meta 
-            title={<a href={item.url}>{item.title}</a>}
-            description={item.description}
-            avatar={item.picto}
-            style={{marginLeft:10}}
-          />
-        </List.Item>
-
-    )}
-    />
-
-  )
-}
 
 export const HomePage:React.FC<PageProps> = () => {
+    const { token } = useToken();
     return (
       <Row gutter={[16, 16]} style={{ margin: 16 }}>
         <Col span={24}>
@@ -79,7 +31,7 @@ export const HomePage:React.FC<PageProps> = () => {
                 </p>
                 <p>
                   Pour toute sollicitation, contactez-nous à{" "}
-                  <Link href="mailto:odema@cerdd.org">
+                  <Link to="mailto:odema@cerdd.org">
                     odema@cerdd.org <FaEnvelope />
                   </Link>.
                 </p>
@@ -88,8 +40,33 @@ export const HomePage:React.FC<PageProps> = () => {
           </Card>  
         </Col>
         <Col md={12} style={{ width: "100%" }}>
-          <Card title="À voir aussi">
-            <ListRelated />
+          <Card title="À voir aussi" style={{height:"100%"}}>
+            <Link to="https://odema-hautsdefrance.org/" style={{ display: "block" }}>
+              <Card
+                hoverable
+                style={{
+                  height: "100%", borderColor: token.colorPrimary,
+                }}
+                styles={{
+                  body: {
+                    height: "100%",display: "flex",
+                    alignItems: "center", justifyContent: "center",
+                  },
+                }}
+              >
+                <Flex vertical align="center" gap={12}>
+                  <Icon icon="gg:website" fontSize={72} color={token.colorPrimary} />
+                  <div
+                    style={{
+                      fontSize: "1.2rem", textAlign: "center",
+                      fontWeight: 500, color:token.colorPrimary
+                    }}
+                  >
+                    Site internet de l’Odema.
+                  </div>
+                </Flex>
+              </Card>
+            </Link>
           </Card>
         </Col>
         <Col md={12}>
@@ -97,7 +74,7 @@ export const HomePage:React.FC<PageProps> = () => {
             <p>
               Le code source de ce tableau de bord est libre et peut être
               consulté sur le{" "}
-              <Link href="https://github.com/geo2france/odema-dashboard">
+              <Link to="https://github.com/geo2france/odema-dashboard">
                 dépot de code <FaGithub />
               </Link>
               .
@@ -105,15 +82,15 @@ export const HomePage:React.FC<PageProps> = () => {
             <p>
               Cette application est alimentée simultanément par des
               données hébergées par{" "}
-              <Link href="https://www.geo2france.fr/">Géo2France</Link> et
-              par l'<Link href="https://data.ademe.fr/">Ademe</Link>. La
+              <Link to="https://www.geo2france.fr/">Géo2France</Link> et
+              par l'<Link to="https://data.ademe.fr/">Ademe</Link>. La
               source est indiquée sous chaque graphique. La plupart des
               données sont sous licence libre et peuvent être réutilisées.
             </p>
             <p>
               Sauf mention contraire, les graphiques présentés ici sont
               sous licence{" "}
-              <Link href="https://creativecommons.org/licenses/by/2.0/fr/deed.fr">
+              <Link to="https://creativecommons.org/licenses/by/2.0/fr/deed.fr">
                 {" "}
                 CC BY <FaCreativeCommons /> <FaCreativeCommonsBy />
               </Link>{" "}
